d81a8629e63d76691c494dbdb80473bf787fc00f
SashaProgrammist <49712254+SashaProgrammist@users.noreply.github.com>
2022-02-27 15:40:34 +0300

    first commit

---
+++ string/string_.h
@@ -0,0 +1,8 @@
+//
+// Created by PC on 27.02.2022.
+//
+
+#ifndef INC_5_STRING__H
+#define INC_5_STRING__H
+
+#endif //INC_5_STRING__H
---
+++ string/test/test.h
@@ -0,0 +1,8 @@
+//
+// Created by PC on 27.02.2022.
+//
+
+#ifndef PROJECT_TEST_H
+#define PROJECT_TEST_H
+
+#endif //PROJECT_TEST_H
